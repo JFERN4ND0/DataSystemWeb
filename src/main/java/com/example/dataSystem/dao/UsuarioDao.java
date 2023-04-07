5998643216f35c4300e3e11eff32e4563bf97abb
FERNANDO <joferguzvas@gmail.com>
2023-04-07 16:56:56 -0600

    Avance del 07042023

---
+++ src/main/java/com/example/dataSystem/dao/UsuarioDao.java
@@ -0,0 +1,9 @@
+package com.example.dataSystem.dao;
+
+import java.util.List;
+
+import com.example.dataSystem.models.User;
+
+public interface UsuarioDao {
+    List<User> getUsuarios();
+}
--- src/main/resources/static/js/tables/tableUser.js
+++ src/main/resources/static/js/tables/tableUser.js
@@ -1,28 +1,34 @@
-$(document).ready(function() {
+$(document).ready(function () {
   cargarUsuarios();
-  $('#table-user').DataTable();
 });
 
-/*
-async function cargarUsuarios() {
-  const rawResponse = await fetch("usuario/123", {
-    method: "GET",
-    headers: {
-      'Accept': "aplication/json",
-      "Content-type": "application/json",
-    },
-  });
-  const usuarios = await rawResponse.json();
-  console.log(usuarios);
-};*/
+// const cargarUsuario = () => {
+//   fetch("usuario/123", {
+//     method: "GET",
+//     headers: {
+//       "Content-type": "application/json",
+//     },
+//   })
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
+// };
 
 const cargarUsuarios = () => {
-fetch('usuario/123',{
-        method: 'GET',
-        body: JSON.stringify(newPost),
-        headers: {
-            "Content-type": "application/json"
-        }
-    }).then(res => res.json())
-    .then(data => console.log(data))
-}
+  let listUser = "";
+
+  fetch("usuarios", {
+    method: "GET",
+    headers: {
+      "Content-type": "application/json",
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      // console.log(data);
+      for (const user in data) {
+        const usuario = `<tr><td>${data[user].id}</td><td>${data[user].nombre_u}</td><td>${data[user].username}</td><td>${data[user].nivel}</td><td>${data[user].estado}</td></tr>`;
+        listUser += usuario;
+      }
+      document.querySelector("#table-user tbody").outerHTML = listUser;
+    });
+};
